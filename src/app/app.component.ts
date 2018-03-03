b241f6ad4b885d1eee64d850c58527deb6e623dd
loe-alamis <loe.alamis@gmail.com>
2018-03-03 20:18:52 +0800

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HubConnection } from '@aspnet/signalr-client';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  private hubConnection: HubConnection;
+
+  nick = '';
+  message = '';
+  messages: string[] = [];
+
+  ngOnInit(){
+    this.nick = window.prompt('Your name: ', 'John');
+    this.hubConnection = new HubConnection('http://localhost:5000/chat');
+
+    this.hubConnection
+      .start()
+      .then(() => console.log('Connection started'))
+      .catch(err => console.log('Error while establishing connection'));
+
+    this.hubConnection.on('sendToAll', (nick: string, receivedMessage: string) => {
+      const text = `${nick}: ${receivedMessage}`;
+      this.messages.push(text);
+    });
+  }
+
+  public sendMessage(): void {
+    this.hubConnection
+      .invoke('sendToAll', this.nick, this.message)
+      .catch(err => console.error(err));
+  }
 }
